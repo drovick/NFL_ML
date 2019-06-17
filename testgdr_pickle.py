ed9bd3ec431c81944aae3b76d0cb8405072aae57
David Rovick Arrojo <drovick96@gmail.com>
2019-06-17 22:39:42 +0100

    added testgdr_pickle.py

--- testgdr_pickle.py
+++ testgdr_pickle.py
@@ -3,8 +3,8 @@ import pickle
 def multitree_get_errors(index,importance_list,error_list,tree_list,tolss,iterss,fracss,subss,ratess,n_estimators_list,maxdep_list,minsamp_split_list,minsamp_leaf_list,minweight_frac_leaf_list,maxfeat_list,maxleaf_nodes_list,minimp_dec_list):
     print('Model:',str(tree_list[index]),', n_estimators:', str(n_estimators_list[index]),', max_depth:', str(maxdep_list[index]), ', min_samples_split:', str(minsamp_split_list[index]))
     print('min_samples_leaf:', str(minsamp_leaf_list[index]), 'min_weight_fraction_leaf:', str(minweight_frac_leaf_list[index]), ', max_features:,', str(maxfeat_list[index]))
-    print('max_leaf_nodes:', str(maxleaf_nodes_list[index]), ', min_impurity_decrease:', str(minimp_dec_list[index]))
-    print('tolerance:', str(tolss[index]),', n_iterations:', str(iterss[index]),', validation_fraction:',str(fracss[index]),', subsample:',str(subss[index]),', learning rate:',str(ratess[index]))
+    print('max_leaf_nodes:', str(maxleaf_nodes_list[index]), ', min_impurity_decrease:',str(minimp_dec_list[index]),' tolerance:', str(tolss[index]))
+    print('n_iterations:', str(iterss[index]),', validation_fraction:',str(fracss[index]),', subsample:',str(subss[index]),', learning rate:',str(ratess[index]))
     return error_list[index]
 
 filename = 'gdr_pickle_test'
